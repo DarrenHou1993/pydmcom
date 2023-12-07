359349798be50712efc10346eb22674c7304b3ba
donglianghou <pillarhou@yeah.net>
2023-12-07 23:04:59 +0800

    测试备注

--- test.py
+++ test.py
@@ -8,7 +8,10 @@ print(dm.GetScreenWidth())
 
 regRet = dm.Reg('6008885359e4a0926d85f7483f16735d9fb71b', 'JINXIN')
 print(f"注册结果: {regRet}")
+# Chrome_RenderWidgetHostHWND
+# 348,31,380,47,宽高(32,16) 清除
+# 高度80后可以绘画
 
-bindRet = dm.BindWindowEx(123321)
+bindRet = dm.BindWindowEx(131320)
 
 print(f"绑定结果: {bindRet}")
